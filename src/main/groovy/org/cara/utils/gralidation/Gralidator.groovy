4f93dfe87dfca3ec01489c2648d829a485b657dd
loiccara <loic.cara@192.com>
2016-02-09 15:49:22 +0000

    code optimizationn for easier maintenance

--- src/main/groovy/org/cara/utils/gralidation/Gralidator.groovy
+++ src/main/groovy/org/cara/utils/gralidation/Gralidator.groovy
@@ -20,16 +20,10 @@ class Gralidator {
 
         objectConstraints.each { String propertyName, Map controls ->
             def propertyValue = object.getProperties().get(propertyName)
-            controls.each { String constraint, def controlValue ->
-                GralidationEnum currentControl = constraint.toUpperCase() as GralidationEnum
-                if (!currentControl.control.call(propertyValue, controlValue)){
-                    errors.add(currentControl.errorCode)
-                }
-            }
+            GralidationResult tempResult = executeControls(propertyValue, controls)
+            errors.addAll(tempResult.errors)
         }
-
-        boolean isValid=errors.size()==0
-        new GralidationResult(isValid: isValid, errors:errors)
+        new GralidationResult(isValid:errors.isEmpty(), errors:errors)
     }
 
     static GralidationResult gralidate(Map myMap, Map constraints, boolean validateInexistantKeys){
@@ -37,17 +31,23 @@ class Gralidator {
         constraints.each { String propertyName, Map controls ->
             if (myMap?.containsKey(propertyName)){
                 def propertyValue = myMap.get(propertyName)
-                controls.each { String constraint, def controlValue ->
-                    GralidationEnum currentControl = constraint.toUpperCase() as GralidationEnum
-                    if (!currentControl.control.call(propertyValue, controlValue)) {
-                        errors.add(ERROR_CODE_PREFIX + currentControl.errorCode)
-                    }
-                }
+                GralidationResult tempResult = executeControls(propertyValue, controls)
+                errors.addAll(tempResult.errors)
             } else if(validateInexistantKeys) {
                 errors.add(ERROR_CODE_PREFIX + "inexistantProperty:" + propertyName)
             }
         }
-        boolean isValid=errors.size()==0
-        new GralidationResult(isValid: isValid, errors:errors)
+        new GralidationResult(isValid:errors.isEmpty(), errors:errors)
+    }
+
+    protected static GralidationResult executeControls(def propertyValue, Map controls){
+        List errors = []
+        controls.each { String constraint, def controlValue ->
+            GralidationEnum currentControl = constraint.toUpperCase() as GralidationEnum
+            if (!currentControl.control.call(propertyValue, controlValue)) {
+                errors.add(ERROR_CODE_PREFIX + currentControl.errorCode)
+            }
+        }
+        new GralidationResult(isValid:errors.isEmpty(), errors:errors)
     }
 }
